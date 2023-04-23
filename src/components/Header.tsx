a9ae3b910ef2ce1d8c64287ef72b23d93bd4a8b9
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-23 16:11:52 -0700

    project setup and mock

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function Header({}: Props) {
+  return (
+    <header>
+      <div>{/* Social Icons */}</div>
+    </header>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Header from "../components/Header";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Ethan's Portfolio</title>
+      </Head>
+
+      {/* Header */}
+      <Header />
+
+      {/* Hero */}
+
+      {/* About */}
+
+      {/* Experience */}
+
+      {/* Skills */}
+
+      {/* Projects */}
+
+      {/* Contact */}
+    </div>
+  );
+};
+
+export default Home;
