62a82523457fe9675add0ee1d311ff67b52a1eb4
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-24 13:09:42 -0700

    complete about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,12 @@ type Props = {};
 
 export default function About({}: Props) {
   return (
-    <div className="flex flex-column relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
@@ -15,14 +20,29 @@ export default function About({}: Props) {
           x: -200,
         }}
         transition={{ duration: 1.2 }}
-        whileInView={{ x: 0 }}
+        whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
         src="https://i.ibb.co/tQ6GQQN/Screenshot-2023-04-24-at-12-30-31-PM.png"
         className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
-      <div>
-        <h4>Here is a little background</h4>
+
+      <div className="space-y-10 px-0 md:px-10">
+        <h4 className="text-4xl font-semibold">
+          Here is a{" "}
+          <span className="underline decoration-[#F7AB0A]/50">little</span>{" "}
+          background
+        </h4>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui
+          vivamus arcu felis bibendum ut tristique et. Metus dictum at tempor
+          commodo ullamcorper a lacus vestibulum sed. Eleifend quam adipiscing
+          vitae proin sagittis nisl rhoncus. Pretium quam vulputate dignissim
+          suspendisse in est. Nibh sed pulvinar proin gravida hendrerit lectus
+          a. Hac habitasse platea dictumst quisque sagittis. Et magnis dis
+          parturient montes nascetur.
+        </p>
       </div>
-    </div>
+    </motion.div>
   );
 }
