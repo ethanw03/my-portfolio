84867a5a1e4adf40b21b1ff660b4c92e3a379e7a
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-24 10:27:45 -0700

    add hero, Y-snapping, and styling for hero background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-row items-center">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center"
+      >
         {/* Social Icons */}
         <SocialIcon
           url="https://github.com/ethanw03"
@@ -23,8 +33,21 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
-      <div className="flex flex-row items-center text-gray-300 ">
+      </motion.div>
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-row items-center text-gray-300 "
+      >
         <SocialIcon
           className="cursor-pointer"
           network="email"
@@ -34,7 +57,7 @@ export default function Header({}: Props) {
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
           Get In Touch
         </p>
-      </div>
+      </motion.div>
     </header>
   );
 }
---
+++ src/components/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+
+type Props = {};
+
+export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "The Name's Ethan Nicholas",
+      "Full-Stack-Developer.tsx",
+      "console.log(Hello World)",
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor="#F7AB0A" />
+      </h1>
+    </div>
+  );
+}
