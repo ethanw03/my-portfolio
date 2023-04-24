772a16f2fdc683e72dfc94addc243c53de18b303
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-23 18:41:47 -0700

    add react social icons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,17 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header>
-      <div>{/* Social Icons */}</div>
+      <div>
+        {/* Social Icons */}
+        <SocialIcon url="https://github.com/ethanw03" />
+        <SocialIcon url="https://www.linkedin.com/in/ethanw03/" />
+        <SocialIcon url="https://www.instagram.com/ethancodes/" />
+      </div>
     </header>
   );
 }
