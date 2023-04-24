4da4ba7fa06a2f829fa3d74ea2cf8ad6f8e99016
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-24 14:39:48 -0700

    complete skills page and add projects page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
+import Projects from "../components/Projects";
 
 const Home: NextPage = () => {
   return (
@@ -40,7 +41,9 @@ const Home: NextPage = () => {
       </section>
 
       {/* Projects */}
-
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact */}
     </div>
   );
