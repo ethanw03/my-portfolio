e9cc219616516885b2dcb198f1e7522a99781501
Ethan Wong <ethanwong98@yahoo.ca>
2023-04-24 09:44:12 -0700

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,12 +5,35 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
-      <div>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
+      <div className="flex flex-row items-center">
         {/* Social Icons */}
-        <SocialIcon url="https://github.com/ethanw03" />
-        <SocialIcon url="https://www.linkedin.com/in/ethanw03/" />
-        <SocialIcon url="https://www.instagram.com/ethancodes/" />
+        <SocialIcon
+          url="https://github.com/ethanw03"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/ethanw03/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.instagram.com/ethancodes/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div className="flex flex-row items-center text-gray-300 ">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400 ">
+          Get In Touch
+        </p>
       </div>
     </header>
   );
