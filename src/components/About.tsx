a819e23df3dad54900ce8800044a23b6f1726e75
ethanw03 <ethanwong98@yahoo.ca>
2023-04-28 00:16:06 -0700

    note: tailwind intellisense only working on wsl2

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { PageInfo } from "../../typings";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { PageInfo } from '../../typings';
 
 type Props = {
   pageInfo: PageInfo;
@@ -12,9 +12,9 @@ export default function About({ pageInfo }: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center '
     >
-      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
         About
       </h3>
 
@@ -25,17 +25,18 @@ export default function About({ pageInfo }: Props) {
         transition={{ duration: 1.2 }}
         whileInView={{ opacity: 1, x: 0 }}
         viewport={{ once: true }}
-        src="https://i.ibb.co/tQ6GQQN/Screenshot-2023-04-24-at-12-30-31-PM.png"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        src='https://i.ibb.co/tQ6GQQN/Screenshot-2023-04-24-at-12-30-31-PM.png'
+        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]'
       />
 
-      <div className="space-y-10 px-0 md:px-10">
-        <h4 className="text-4xl font-semibold">
-          Here is a{" "}
-          <span className="underline decoration-[#36454F]/50">little</span>{" "}
+      <div className='space-y-10 px-0 md:px-10'>
+        <h4 className='text-4xl font-semibold'>
+          
+          Here is a{' '}
+          <span className='underline decoration-[#36454F]/50'>little</span>{' '}
           background
         </h4>
-        <p className="text-base">{pageInfo?.backgroundInformation}</p>
+        <p className='text-base'>{pageInfo?.backgroundInformation}</p>
       </div>
     </motion.div>
   );
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,7 +11,7 @@ export default function Projects({ projects }: Props) {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className=" h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+      className=" h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full  xl:text-red-500 justify-evenly mx-auto items-center z-0"
     >
       <h3 className="absolute top-20 md:top-24 uppercase tracking-[20px] text-gray-500 text-xl md:text-2xl">
         Projects
